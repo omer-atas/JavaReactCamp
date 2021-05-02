b0b1d4491a114a43043b49359232d20ab672b108
Ömer ATAŞ <79022360+omer-atas@users.noreply.github.com>
2021-05-02 18:43:36 +0300

    Add files via upload

--- JavaReactCamp/src/JavaReactCampWork4/Main.java
+++ JavaReactCamp/src/JavaReactCampWork4/Main.java
@@ -26,24 +26,12 @@ public class Main {
 		playerManager.userValidation(player);
 		playerManager.playerAdd(player);
 		
-		Player player2 = new Player();
-		player2.setId(2);
-		player2.setIdentityNumber("999999999");
-		player2.setFirstName("Yunus");
-		player2.setLastName("Kilic");
-		player2.setBirtDay("06.09.1999");
-		
-		PlayerManager playerManager2 = new PlayerManager();
-		playerManager2.userValidation(player2);
-		playerManager2.playerAdd(player2);
-		
+	
 		Sale sale = new Sale(1,campaign,player);
 		
 		SaleManager saleManager = new SaleManager();
 		saleManager.playerJoinCampaign(player, campaign);
 		
-		SaleManager saleManager2 = new SaleManager();
-		saleManager2.playerOutCampaign(player2, campaign);
 	}
 
 }
--- JavaReactCamp/src/JavaReactCampWork4/Utils.java
+++ JavaReactCamp/src/JavaReactCampWork4/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
 	public static void check(){
 		if(PlayerManager.sayac != 0) {
 			System.out.println("Ýslem gerceklestiriliyor..");
-			
+			PlayerManager.sayac = 0;
 		}else if(PlayerManager.sayac <= 0){
 			System.out.println("Ýslem gerceklestirilemedi...");
 			System.exit(0);
